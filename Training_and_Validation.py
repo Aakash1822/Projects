14e32818e023f63b2dd87c2ca4ee8126e23f5e62
Aakash Singh <30810386+Aakash1822@users.noreply.github.com>
2019-09-18 20:56:52 +0530

    Accuracy evaluation

---
+++ Training_and_Validation.py
@@ -0,0 +1,20 @@
+# Evalutaing accuracy of Training and Validation
+
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,metrics=["accuracy"])
+
+history = model.fit_generator(
+    aug.flow(x_train, y_train, batch_size=BS),
+    validation_data=(x_test, y_test),
+    steps_per_epoch=len(x_train) // BS,
+    epochs=EPOCHS, verbose=1
+    )
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'b', label='Training accurarcy')
+plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
+plt.title('Training and Validation accurarcy')
+plt.legend()
