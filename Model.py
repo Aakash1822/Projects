2dc2e8c5f0e3e67270ba4e5a8189fbc68d1ea6a0
Aakash Singh <30810386+Aakash1822@users.noreply.github.com>
2019-09-18 20:53:38 +0530

    Model.py
    Using Deep Learning (Convolutional Neural network) 2D Model is made.

---
+++ Model.py
@@ -0,0 +1,44 @@
+np_image_list = np.array(image_list, dtype=np.float16) / 225.0
+
+x_train, x_test, y_train, y_test = train_test_split(np_image_list, image_labels, test_size=0.2, random_state = 42) 
+
+aug = ImageDataGenerator(
+    rotation_range=25, width_shift_range=0.1,
+    height_shift_range=0.1, shear_range=0.2, 
+    zoom_range=0.2,horizontal_flip=True, 
+    fill_mode="nearest")
+model = Sequential()
+inputShape = (height, width, depth)
+chanDim = -1
+if K.image_data_format() == "channels_first":
+    inputShape = (depth, height, width)
+    chanDim = 1
+model.add(Conv2D(32, (3, 3), padding="same",input_shape=inputShape))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=chanDim))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=chanDim))
+model.add(Conv2D(64, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=chanDim))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(128, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=chanDim))
+model.add(Conv2D(128, (3, 3), padding="same"))
+model.add(Activation("relu"))
+model.add(BatchNormalization(axis=chanDim))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation("relu"))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(n_classes))
+model.add(Activation("softmax"))
+model.summary()
